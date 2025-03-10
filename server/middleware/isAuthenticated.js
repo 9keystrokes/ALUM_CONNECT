a6f8497925b0f608b7bc805b128c30ee25e6228a
Nayan Mandal <1.nayanmandal@gmail.com>
2025-03-11 03:14:47 +0530

    Delete server directory

--- server/middleware/isAuthenticated.js
+++
@@ -1,9 +0,0 @@
-// server/middleware/isAuthenticated.js
-module.exports = {
-  isAuthenticated: function(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    return res.status(401).json({ message: 'Not authenticated' });
-  }
-};
